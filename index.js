7949911a39b0407ac198d668f0392b0157779ad1
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2022-09-28 17:08:44 -0400

    Completed tasks 5 & 6. All tsks complete

--- index.js
+++ index.js
@@ -182,8 +182,25 @@ class Instructor extends Lambdasian {
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
 
-class Student {
-   
+class Student extends Lambdasian{
+   constructor({name, age, location, previousBackground, className, favSubjects}) {
+    super({name, age, location, previousBackground, className, favSubjects});
+    this.previousBackground = previousBackground;
+    this.className = className;
+    this.favSubjects = favSubjects;
+   }
+
+   listSubjects() {
+    return `Loving ${this.favSubjects}!`;
+   }
+
+   PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+   }
+
+   sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+   }
 }
 
 /*
@@ -200,9 +217,21 @@ class Student {
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
-class ProjectManager {
-   
-}
+class ProjectManager extends Instructor{
+   constructor({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}) {
+    super({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor});
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+   }
+
+   standUp(channel) {
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+   }
+
+   debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+   }
+   }
 
 /*
   STRETCH PROBLEM (no tests!)
